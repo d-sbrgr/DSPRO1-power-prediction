07da9242748bb306d572ff2c9317c83928088900
David Schurtenberger <schurtenberger.david@gmail.com>
2024-10-17 10:55:15 +0200

    chore: only with non NaN data, but for all targets

--- power_prediction/time_series_decomposition.py
+++ power_prediction/time_series_decomposition.py
@@ -11,6 +11,14 @@ def df_to_prophet(df: pd.DataFrame, key: str):
     return pd.DataFrame({"ds": df["Date"], "y": df[key]})
 
 
+def get_start_and_end_dates_for_key(df: pd.DataFrame, key: str) -> tuple[pd.DataFrame, pd.DataFrame]:
+    df = df.copy()
+    df['start'] = (df[key] == 1) & (df[key].shift(1, fill_value=0) == 0)
+    df['end'] = (df[key] == 1) & (df[key].shift(-1, fill_value=0) == 0)
+
+    return df.loc[df['start'], 'Date'].reset_index(drop=True), df.loc[df['end'], 'Date'].reset_index(drop=True)
+
+
 def fit_and_predict_prophet(instance: Prophet, df: pd.DataFrame) -> pd.DataFrame:
     instance.fit(df)
     future = instance.make_future_dataframe(12, freq="ME")
